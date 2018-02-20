195bae2f86e8a0361b47671add476339f029a0b5
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-02-19 22:46:51 -0600

    Add bootstrap script
    Adds a script installing all the pre-requisite software and generating
    an SSH key if needed.
    
    Fixes #13

---
+++ kubernetes/bootstrap/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+# Install Brew.
+brew --version || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Git.
+brew update
+
+# Install brew formulas.
+brew install \
+  brew-cask-completion \
+  bash-completion \
+  docker-completion \
+  git \
+  node \
+  pip-completion 2>/dev/null
+
+# Install cask formulas.
+brew cask install \
+  docker \
+  iterm2 \
+  virtualbox 2>/dev/null
+
+# Generate an SSH key (without passphrase) if needed and add it to the ssh-agent.
+SSH_PRIVATE_KEY=~/.ssh/id_rsa
+if [ ! -f "${SSH_PRIVATE_KEY}" ]; then
+  ssh-keygen -b 4096 -t rsa -f ${SSH_PRIVATE_KEY} -q -N ""
+  eval "$(ssh-agent -s)"
+  ssh-add -K ${SSH_PRIVATE_KEY}
+fi
+
+# Copies the contents of the public SSH key to your clipboard.
+pbcopy < "${SSH_PRIVATE_KEY}.pub"
