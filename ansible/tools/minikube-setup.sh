c2882ff640b4ffebb6bd9edc74aa92cb3b1a08ae
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-05-09 12:01:32 -0500

    Ansible-ize (#29)
    Use Ansible to manage all infrastructure
    
    * Scaffolding
    * Improve Makefile
    * Add kubernetes-module role
    * Import tools
    * Add playbooks
    * Add .editorconfig file

---
+++ ansible/tools/minikube-setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -euo pipefail
+
+# Define variables.
+C_GREEN='\033[32m'
+C_RED='\033[31m'
+C_RESET_ALL='\033[0m'
+
+# Install packages if needed.
+minikube version || brew cask install minikube
+helm version -c || brew install kubernetes-helm
+
+# Install addons.
+mkdir -p ~/.minikube/addons
+
+# (Re)Start the service.
+# shellcheck disable=SC1083
+MINIKUBE_STATUS=$(minikube status --format  {{.MinikubeStatus}} || true)
+if [ "${MINIKUBE_STATUS}" == "Paused" ] || [ "${MINIKUBE_STATUS}" == "Stopped" ] || [ -z "${MINIKUBE_STATUS}" ]; then
+  echo -e "${C_GREEN}Starting Minikube...${C_RESET_ALL}"
+  minikube start --vm-driver virtualbox --memory 4096 --cpus 4
+elif [ "${MINIKUBE_STATUS}" == "Running" ]; then
+  echo -e "${C_GREEN}Minikube is already running.${C_RESET_ALL}"
+else
+  echo -e "${C_RED}Minikube is in an unknown state.${C_RESET_ALL}"
+  echo -e "${C_RED}Please run 'minikube status' and fix the problem manually.${C_RESET_ALL}"
+  exit 1
+fi
+
+# Set up Minikube context.
+echo -e "${C_GREEN}Configuring minikube context...${C_RESET_ALL}"
+kubectl config use-context minikube
+
+# Set up Helm.
+while ! helm init --wait 2>/dev/null; do
+  echo -e "${C_RED}Helm init has not completed yes...${C_RESET_ALL}"
+  sleep 1
+done
+
+# Workaround for Helm list issues:
+#    https://github.com/kubernetes/helm/issues/2872
+#    https://github.com/kubernetes/helm/issues/3816
+#    https://github.com/kubernetes/helm/issues/3870
+#    https://github.com/kubernetes/helm/issues/3986
+#    https://stackoverflow.com/questions/49747271
+kubectl --namespace=kube-system patch deployment/tiller-deploy --patch '{"spec": {"template": {"spec": {"automountServiceAccountToken": true}}}}'
+
+# This is a hack to ensure tiller is ready.
+echo -ne "${C_GREEN}Waiting for Tiller to start..."
+while ! helm ls 2>/dev/null; do
+  echo -ne "."
+  sleep 1
+done
+echo -e "${C_RESET_ALL}"
+
+# Display a message to tell to update the environment variables.
+minikube docker-env
+
+# Enable addons
+minikube addons enable heapster
+minikube addons enable ingress
+
+# Start the dashboard.
+echo -e -n "${C_GREEN}The dashboard will be displayed in a new tab in the default browser...${C_RESET_ALL}"
+until minikube dashboard >/dev/null 2>&1; do
+  echo -e -n "${C_GREEN}.${C_RESET_ALL}"
+  sleep 1
+done
+echo
